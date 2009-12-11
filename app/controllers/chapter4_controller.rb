bf1b5026a9f1eefabda6bab9223bfa45462bdfe6
Sebastian Obermueller <noodles543@gmail.com>
2009-12-11 15:00:45 +0100

    completed chapter 5

--- app/controllers/chapter4_controller.rb
+++ app/controllers/chapter4_controller.rb
@@ -2,4 +2,12 @@ class Chapter4Controller < ApplicationController
   def index
   end
 
+  def receive_drop
+    render :text => "Sie haben das Element mit der ID #{params[:id]} losgelassen."
+  end
+
+  def repeat
+    render :text => params.inspect
+  end
+
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+Element.addMethods({
+    upcase: function(element){
+        if (!(element = $(element))) return;
+        element.update(element.innerHTML.toUpperCase());
+        return element;
+    },
+
+    toggleClassName: function(element, className) {
+        if (!(element = $(element))) return;
+        element.hasClassName(className) ?
+            element.removeClassName(className) :
+            element.addClassName(className);
+        return element;
+    }
+});
